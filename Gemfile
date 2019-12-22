ba75908a97ec4f153587e8695e0ad4ce5c68fa62
watanabe atsushi <nabesan07610057@gmail.com>
2019-12-22 17:00:17 +0900

    cancancan,rails_adminで権限管理を実装

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'kaminari'
+
+gem 'cancancan'
+
+gem 'rails_admin'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_063517) do
+ActiveRecord::Schema.define(version: 2019_12_22_074028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_12_22_063517) do
     t.integer "support_history"
     t.text "introduce", default: "よろしくお願いします！"
     t.string "icon", default: ""
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
