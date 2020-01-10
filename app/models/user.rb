43fc83f98bf61805e52832b233d54cb3ef6d12ae
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-10 13:56:34 +0900

    モデルにバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   has_many :participant_managements, foreign_key: :participant_id, dependent: :destroy
 
   mount_uploader :icon, ImageUploader
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  validates :introduce, length: { maximum: 150 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
