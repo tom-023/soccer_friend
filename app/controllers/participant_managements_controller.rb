32794d1b2d3b9d0f3a026e7e9448cbc900da8e96
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-12 17:34:06 +0900

    イベント参加者通知メールの自動送信機能を実装

--- app/controllers/participant_managements_controller.rb
+++ app/controllers/participant_managements_controller.rb
@@ -1,8 +1,11 @@
 class ParticipantManagementsController < ApplicationController
 
   def create
+    @event = Event.find(params[:event_id])
+    @user = @event.organizer
     participant_management = current_user.participant_managements.create(event_id: params[:event_id])
     redirect_to events_url, notice: "#{participant_management.event.organizer.name}さんのイベントに参加しました"
+    EventMailer.event_mail(@user, @event).deliver
   end
 
   def destroy
