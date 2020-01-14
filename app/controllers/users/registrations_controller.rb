ae4ddd87cbfab8dfdd5c39739cdaabb94c4f28ef
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-14 12:32:01 +0900

    twitter認証機能を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -70,7 +70,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    root_path
+  end
 end
