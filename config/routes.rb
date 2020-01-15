59b587dcf2f921879521f9a6af7655063b41f3af
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-15 13:20:45 +0900

    プロフィール編集時のパスワードバリデーションを修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  devise_for :users, controllers: { registrations: 'users/registrations' },
-                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-                     
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    omniauth_callbacks: 'users/omniauth_callbacks'
+                                  }
+
   resources :users, only: [:show]
 
   resources :events do
