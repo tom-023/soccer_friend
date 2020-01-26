1888a0f4aeb30ffc250df3b008fe7a588b09bd71
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-26 10:53:17 +0900

    README記載修正

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @conversations = Conversation.all
+    @conversations = Conversation.select(:sender_id, :recipient_id)
   end
 
   def create
