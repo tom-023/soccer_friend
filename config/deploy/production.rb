c378ede6955c0ff75a2d96fbd8075e5520a1a8d3
watanabe atsushi <nabesan07610057@gmail.com>
2020-01-31 12:51:36 +0900

    S3サーバー利用のための設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,12 @@
-server '52.193.15.251', user: 'appwata', roles: %w{app db web}
+server '52.193.15.251',user: 'appwata', roles: %w{app db web}
 set :ssh_options, keys: '/Users/watanabeatsushi/.ssh/id_rsa'
+
+# server '52.193.15.251',
+#       user: 'appwata',
+#       roles: %w{app db web},
+#       ssh_options: {
+#           port: 22,
+#           user: "appwata",
+#           keys: '/Users/watanabeatsushi/.ssh/id_rsa',
+#           forward_agent: true
+#       }
