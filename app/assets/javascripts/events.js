3225bcef2763322b69fbd6a7e38c8b5111b1c7bc
watanabe atsushi <guanlidubian@gmail.com>
2020-02-08 20:22:59 +0900

    検索結果画面の開閉ボタンを移動

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function () {
   $('.list-group-toggle').hide();
-  $('.card-img h3').click(function () {
-    let index = $('.card-img h3').index($(this));
+  $('.slideToggle').click(function () {
+    let index = $('.slideToggle').index($(this));
     $('.list-group-toggle').eq(index).slideToggle();
   });
 });
